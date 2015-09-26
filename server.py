8c2d8a6d4e72463e5f1463c48cedaa91eb1cb242
rtrad <trad.ryan.alan@gmail.com>
2015-09-26 03:45:07 -0400

    minor changes to server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from config import oauth_params
 from config import twitter_api_params
+import json
 import requests
 from requests_oauthlib import OAuth1
 
@@ -9,10 +10,27 @@ class PrintServer():
         self.auth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret)
         self.session = requests.Session()
 
+    def _get_user(self, handle):
+        handle = json.loads(self.session.get('https://api.twitter.com/1.1/users/lookup.json', auth=self.auth, params={'screen_name':handle}).text)
+        found = None
+        for entry in handle:
+            if 'id' in entry:
+                found = entry
+        if not found is None:
+            return found
+        return None
+
     def run(self):
         print 'establishing connection to Twitter API'
-        params = {'follow':twitter_api_params['printer_handle'], 'track':'print', 'stringify_friend_ids':'true'}
+        
+        printer = self._get_user(twitter_api_params['printer_handle'])
+        if printer is None:
+            print 'Error: could not find username "{0}"'.format(twitter_api_params['printer_handle'])
+            return
+        
+        params = {'follow':printer['id']}
         response = self.session.get(url=twitter_api_params['url'], auth=self.auth, params=params, stream=True)
         for line in response.iter_lines():
-            with open('posts.json', 'wb+') as fw:
-                fw.write(line)
+            print line
+            # with open('posts.json', 'wb+') as fw:
+                # fw.write(line)
