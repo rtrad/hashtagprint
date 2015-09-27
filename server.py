20e215a6b165e9c4421c0375f94cee4ba76d0126
rtrad <trad.ryan.alan@gmail.com>
2015-09-27 08:20:25 -0400

    minor bug fixes

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import config
 import json
-import ast
 import requests
 import thread
 import random
@@ -74,8 +73,8 @@ class PrintServer():
                 return
             elif 'web' in hashtags:
                 if 'entities' in post.getRaw() and 'urls' in post.getRaw()['entities']:
-                    print post.getRaw()['entities']['urls'][0]['expanded_url']
-                    self._print_web(post.getRaw()['entities']['urls'][0]['display_url'])
+                    print type(post.getRaw()['entities']['urls'][0]['expanded_url'])
+                    self._print_web(post.getRaw()['entities']['urls'][0]['expanded_url'])
                 else:
                     params = {'status':'{1} @{0}, you must include a url for #web'.format(post.getSender(), ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6)))}
                     requests.post(url='https://api.twitter.com/1.1/statuses/update.json', auth=self.auth, data=params)
