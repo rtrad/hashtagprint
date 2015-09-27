8ce4e0ea0eb035eb979d1c28c7961865863cfc8f
rtrad <trad.ryan.alan@gmail.com>
2015-09-27 02:31:38 -0400

    minor changes to init

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
-import config
 from server import PrintServer
 
 def main():
     print_server = PrintServer()
+    print_server.load_users('valid_users.txt')
     print_server.run()
 
 if __name__ == '__main__':
